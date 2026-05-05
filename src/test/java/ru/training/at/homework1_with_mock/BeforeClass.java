afbcbe69b7ce3b89f3460398195c67275c827781
Julia Zhdanova <yulya_zhdanova_1992@mail.ru>
2026-05-05 21:05:20 +0300

    16th commit

--- src/test/java/ru/training/at/homework1_with_mock/BeforeClass.java
+++ src/test/java/ru/training/at/homework1_with_mock/BeforeClass.java
@@ -1,14 +1,14 @@
-package ru.training.at.homework1_with_mock;
-
-import com.epam.tat.module4.Calculator;
-
-public class BeforeClass {
-
-    public Calculator calculator;
-
-    @org.testng.annotations.BeforeClass
-    public void setUp(){
-        calculator = new Calculator();
-    }
-
-}
+//package ru.training.at.homework1_with_mock;
+//
+//import com.epam.tat.module4.Calculator;
+//
+//public class BeforeClass {
+//
+//    public Calculator calculator;
+//
+//    @org.testng.annotations.BeforeClass
+//    public void setUp(){
+//        calculator = new Calculator();
+//    }
+//
+//}
